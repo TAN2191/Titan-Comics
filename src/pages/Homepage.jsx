caadb87ab0132a2ae7c7e4a80f238e62bcd2fd4f
Tanishq Rajak <78699111+TAN2191@users.noreply.github.com>
2023-11-23 20:56:08 +0530

    Update Homepage.jsx

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -13,7 +13,7 @@ export default function HomePage() {
                 <div class="row-span-1 h-[10%]">
                     <Navbar />
                 </div>
-                <div class="md:flex">
+                <div class=" flex justify-center md:flex">
                     <div class="md:w-1/2 p-4 my-auto">
                         <div class="mt-40 inline-block">
                             <p className="text-gray-300 mx-2 text-5xl font-bubblegum tracking-wide font-extrabold md:text-left lg:text-8xl md:text-12xl">
