4b72a6e36ae5359723358266dbbc2d490272876b
Tanishq Rajak <78699111+TAN2191@users.noreply.github.com>
2023-11-23 21:07:16 +0530

    Update Homepage.jsx

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,6 @@
 import Typewriter from "typewriter-effect";
 import Navbar from "../components/Navbar";
+import { Link, Outlet } from "react-router-dom";
 
 export default function HomePage() {
 
@@ -43,10 +44,10 @@ export default function HomePage() {
                     </div>
                     <div class="px-8 pt-2 pb-4 max-w-screen-md mx-auto">
                         <button 
-                            onClick={onClick} 
-                            class="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-                        >
-                            Let's Start
+                            class="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+                            <Link to="/comic-strip" class="block py-2 x-2 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                                    Let's Start
+                            </Link>  
                         </button>
                     </div>
                 </div>
